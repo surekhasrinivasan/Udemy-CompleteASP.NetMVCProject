70262d3b5544d888394023ed1ea502d8804432d0
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-11 06:20:51 -0400

    Learnt some of the Action Results and its helper methods

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,11 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
