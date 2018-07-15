db8db7e1ef49196c604cbc7cf4fe87f30b95bc50
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-15 07:09:33 -0400

    created Views/Customers/Index.cshtml

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customers
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public CustomersController()
         {
-            return View();
+            _context = new ApplicationDbContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ViewResult Index()
+        {
+            //var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer { Id = 1, Name = "John Smith" },
+        //        new Customer { Id = 2, Name = "Mary Williams"}
+        //    };
+        //}
     }
 }
