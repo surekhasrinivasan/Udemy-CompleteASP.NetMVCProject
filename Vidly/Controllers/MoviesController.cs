067fe5056f37e9123d756d079ed46af7682aed42
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-16 17:12:01 -0400

    Exercise-2 - Worked on MoviesController.cs

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,21 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-E" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -27,34 +42,6 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id = 1" + id);
-        }
-
-        //this action will be called when we navigate to //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        //apply the route attribute and pass the url template, can also use regex constraints, with attributes we can other constraints like range 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
         }
     }
-}
+ }
